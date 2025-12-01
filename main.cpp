0ffd5be6b141e3ee2c7390c63594c379e5c51eb3
kirusha-13 <kirillukashevich2007@gmail.com>
2025-12-01 17:52:45 +0300

    End classwork

--- main.cpp
+++ main.cpp
@@ -26,18 +26,54 @@ namespace top{
     p_t o;
     Dot(int x, int y);
   };
+  struct frame_t {
+  p_t left_bot;
+  p_t right_top;
+  };
+  void make_f(IDraw** b, size_t k){}
+  void get_points(IDraw * d, p_t ** ps, size_t & s){}
+  frame_t build_frame(const p_t * ps, size_t s){}
+  char * build_canvas(frame_t f){}
+  void paint_canvas(char * cnv, frame_t fr, const p_t * ps, size_t k, char f){}
+  void print_canvas(const char * cnv, frame_t fr){}
 }
 
-top::Dot::Dot(int x, int y):
-IDraw(), o{x, y} {}
-top::p_t top::Dot::begin() const{
-  return o;
-}
-top::p_t top::Dot::next(p_t) const {
-  return begin();
-}
 
 int main()
 {
   using namespace top;
+  int err = 0;
+  IDraw* f[3] = {};
+  p_t * p = nullptr;
+  size_t s = 0;
+  char * cnv = nullptr;
+  try{
+    make_f (f, 3);
+    for(size_t i = 0; i < 3; ++i){
+      get_points(f[i], kp, s);
+    }
+    frame_t fr = build_frame(p, s);
+    cnv = build_canvas(fr);
+    paint_canvas(cnv, fr, p, s, '#');
+    print_canvas(cnv, fr);
+  } catch(...){
+    err = 1;
+    delete f[0];
+    delete f[1];
+    delete f[2];
+    delete[] p;
+    delete[] cnv;
+    return err;
+  }
+}
+
+top::Dot::Dot(int x, int y):
+IDraw(), o{x, y} {}
+
+top::p_t top::Dot::begin() const{
+  return o;
+}
+
+top::p_t top::Dot::next(p_t) const {
+  return begin();
 }
