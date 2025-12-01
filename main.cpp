ba415b0b175c8151c712b9a967fb2d68ed73019b
kirusha-13 <kirillukashevich2007@gmail.com>
2025-12-01 17:17:19 +0300

    Dot

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ namespace top{
   };
 
   struct IDraw {
-    virtual p_t begin() = 0;
-    virtual p_t next() = 0;
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
   };
 
   bool operator == (p_t a, p_t b){
@@ -19,6 +19,22 @@ namespace top{
   bool operator != (p_t a, p_t b){
     return !(a == b);
   }
+
+  struct Dot:IDraw{
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
+  };
+}
+
+top::Dot::Dot(int x, int y):
+IDraw(), o{x, y} {}
+top::p_t top::Dot::begin() const{
+  return o;
+}
+top::p_t top::Dot::next(p_t) const {
+  return begin();
 }
 
 int main()
